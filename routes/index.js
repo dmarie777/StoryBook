b9d2efea4caa55f3b968e8205426af178155fb80
dmarie777 <deimarie777@gmail.com>
2023-10-03 00:21:47 -0500

    Creation of error files in Views

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
+
+const Story = require('../models/Story')
 // @desc  Login/Landing page
 // @route GET /
 router.get('/',  ensureGuest, (req, res) => {
@@ -13,11 +15,14 @@ router.get('/',  ensureGuest, (req, res) => {
 // @route GET /dashboard
 router.get('/dashboard',  ensureAuth, async (req,res) => {
     try {
+        const stories = await Story.find({ user: req.user.id }).lean()
         res.render('dashboards', {
             name: req.user.firstName,
+            stories
         }) 
     } catch (err) {
         console.error(err)
+        res.render('error/500')
     }
 })
 
