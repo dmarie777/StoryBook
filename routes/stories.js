4973bee2c3878cc5d0ede9ac9967927a4e580c35
dmarie777 <deimarie777@gmail.com>
2023-10-03 16:26:02 -0500

    Implementation of the index.hbs file

--- routes/stories.js
+++ routes/stories.js
@@ -10,6 +10,8 @@ router.get('/add',  ensureAuth, (req, res) => {
     res.render('stories/add')
 })
 
+// @desc  Process add form
+// @route POST / stories
 router.post ('/', ensureAuth, async (req, res) => {
     try{
         req.body.user = req.user.id
@@ -20,5 +22,24 @@ router.post ('/', ensureAuth, async (req, res) => {
         res.render('error/500')
     }
 } )
+
+// @desc Show all stories
+// @route GET / stories
+router.get('/',  ensureAuth, async(req, res) => {
+    try {
+        const stories = await Story.find({ status: 'public' })
+        .populate('user')
+        .sort({ createAt: 'desc' })
+        .lean()
+
+        res.render('stories/index', {
+            stories,
+        })
+    } catch (err) {
+        console.log(err)
+        res.render('error/500')
+    }
+})
+
 
 module.exports = router
